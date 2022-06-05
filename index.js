56175f88bf9f1204ce0f516a877ef39f801f01af
GeoffreyOgembo <geoffereyogembo06@gmail.com>
2022-06-05 11:06:48 -0400

    commit tree 2595c6908a54c791172fe5faf39103ee58ae49a6

---
+++ index.js
@@ -0,0 +1,13 @@
+function introduction(name) {
+    console.log("Hi, my name is ${name}.")
+}
+function introductionWithLanguage(name, language) {
+
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+
+}
+function introductionWithLanguage(name, language="Javascript) {
+    
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+   
+    introductionWithLanguageOptional(Gracie)
